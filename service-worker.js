80e09f6845944febc952945e0f823abbb24178e4
Aryan Gupta <aryanguptaleo@gmail.com>
2020-07-07 13:08:42 +0530

    t3 test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6869b9c1a13f08ee255111302bc6d94e.js"
+  "/precache-manifest.d01cf7dd9a7e920c8ee8041836fe26b3.js"
 );
 
 self.addEventListener('message', (event) => {
